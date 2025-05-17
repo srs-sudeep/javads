da52447e3ed35064cbfbd17391ff2d79d99e829d
srs-sudeep <sudeepranjan@iitbhilai.ac.in>
2025-05-17 10:11:24 +0530

    feat: added method example

---
+++ Methods/MethodEx.java
@@ -0,0 +1,9 @@
+public class MethodEx {
+    public void message(){
+        System.out.println("Hello World");
+    }
+    public static void main (String[] args){
+        MethodEx obj = new MethodEx();
+        obj.message();
+    }
+}
