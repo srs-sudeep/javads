23b34237458d7b71dcf67142df6f0231b0d4ba1f
srs-sudeep <sudeepranjan@iitbhilai.ac.in>
2025-05-17 09:57:48 +0530

    feat : Break

---
+++ Break.java
@@ -0,0 +1,15 @@
+public class Break{
+    public static void main (String[] args){
+        int n = 10;
+        switch(n){
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            default:
+                System.out.println("3");
+        }
+    }
+}
