3c9ab678dd7c508735483356e7890a8114ad1dc7
srs-sudeep <sudeepranjan@iitbhilai.ac.in>
2025-05-17 09:30:42 +0530

    feat : if else

---
+++ Ifelse.java
@@ -0,0 +1,9 @@
+public class Ifelse {
+    public static void main(String[] args){
+        int i = 10;
+        if(i<15){
+            System.out.println("i is smaller than 15");
+        }
+        System.out.println("Outside the block");
+    }
+}
