5b3014bdff5a76074d52eeb734bdea17dddd0b65
srs-sudeep <sudeepranjan@iitbhilai.ac.in>
2025-05-19 11:08:58 +0530

    feat: MultiDim Array

---
+++ Arrays/Multidim.java
@@ -0,0 +1,7 @@
+public class Multidim {
+    public static void main (String[] args){
+        int[][] arr =  new int[3][3];
+        arr[2][1] = 20;
+        System.out.println(arr[2][1]);
+    }
+}
